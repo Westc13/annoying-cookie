91f94080d4030d10ff80daa9b8ad7505af5f6bc1
ChristineWest <christinewwwest@gmail.com>
2022-09-30 17:22:38 -0400

    using JS to get modal element to disappear when modal-close-btn is clicked

--- script.js
+++ script.js
@@ -42,3 +42,12 @@ const modal = document.getElementById("modal");
 setTimeout(function () {
   modal.style.display = "inline";
 }, 1500);
+
+//TODO: Challenge
+// 1. Take control of the close button.
+// 2. Use an event listener to set the display property of the modal to 'none' when the close button is clicked.
+
+const modalCloseBtn = document.getElementById("modal-close-btn");
+modalCloseBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+});
