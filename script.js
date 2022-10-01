e7477e180b0d19697bc4d9fa1d0565ccf07ea089
ChristineWest <christinewwwest@gmail.com>
2022-10-01 11:43:01 -0400

    added buttons and prevent default to form

--- script.js
+++ script.js
@@ -51,3 +51,15 @@ const modalCloseBtn = document.getElementById("modal-close-btn");
 modalCloseBtn.addEventListener("click", function () {
   modal.style.display = "none";
 });
+
+//TODO: Challenge
+// 1. Take control of the form element.
+// 2. Add an eventListener to the form to listen for a 'submit' event.
+// 3. When a user clicks 'accept', prevent the default behavior that triggers the refresh.
+// 4. Log out 'form submitted'.
+
+const consentForm = document.getElementById("consent-form");
+consentForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("form submitted");
+});
